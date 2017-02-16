a74879364378803d8aba0558fcb4644c5983d927
vinicius.matheus252@gmail.com <vinicius.matheus252@gmail.com>
2017-02-15 23:55:57 -0300

    Geração de arquivos de saída pelo script

--- run.sh
+++ run.sh
@@ -4,11 +4,16 @@ if [ -d "$1" ]
 	then
 		for file in "$1"/*".in"
 		do
-			echo "$file"
-			java Main < "$file"
+			echo "Ordenando $file"
+			arquivo="${file##*/}"
+			java Main < "$file" > "resultados/"${arquivo%.*}".out"
+			echo "Resultado salvo em \'""resultados/"${arquivo%.*}".out""\'"
 		done
 	else
-		java Main < "$1"
+		echo "Ordenando $1"
+		file="${1##*/}"
+		java Main < "$1" > "resultados/"${file%.*}".out"
+		echo "Resultado salvo em '""resultados/"${file%.*}".out""'"
 fi
 
 
